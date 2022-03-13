4a2b85b6b1f50a4b22751aa022526592f29515ea
Aryna <Aryna_Palianskaya@epam.com>
2022-03-13 11:32:49 +0300

    added Add-edit buttons for academic titles

--- src/main/java/com/bntu/timetable/controller/AcademicTitleController.java
+++ src/main/java/com/bntu/timetable/controller/AcademicTitleController.java
@@ -5,10 +5,7 @@ import com.bntu.timetable.service.AcademicTitleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,17 @@ public class AcademicTitleController {
     @PreAuthorize("hasAuthority('titles:read')")
     public List<AcademicTitle> getAcademicTitles() {
         return academicTitleService.getAcademicTitles();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('titles:create')")
+    public AcademicTitle createAcademicTitle(@RequestBody AcademicTitle academicTitle) {
+        return academicTitleService.createAcademicTitle(academicTitle);
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAuthority('titles:update')")
+    public AcademicTitle updateAcademicTitle(@RequestBody AcademicTitle academicTitle) {
+        return academicTitleService.updateAcademicTitle(academicTitle);
     }
 }
