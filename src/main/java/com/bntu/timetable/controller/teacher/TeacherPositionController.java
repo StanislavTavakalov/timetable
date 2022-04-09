a12329dec621a87d38f44b6eecde4055fc6cbaa8
Aryna <Aryna_Palianskaya@epam.com>
2022-04-09 15:17:54 +0300

    added crud for workTariffs and teacherPositions

--- src/main/java/com/bntu/timetable/controller/teacher/TeacherPositionController.java
+++ src/main/java/com/bntu/timetable/controller/teacher/TeacherPositionController.java
@@ -4,13 +4,13 @@ import com.bntu.timetable.entity.teacher.TeacherPosition;
 import com.bntu.timetable.service.api.teacher.TeacherPositionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -25,5 +25,29 @@ public class TeacherPositionController {
     @PreAuthorize("hasAuthority('positions:read')")
     public List<TeacherPosition> getTeacherPositions() {
         return teacherPositionService.getTeacherPositions();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('positions:create')")
+    public TeacherPosition createTeacherPosition(@RequestBody TeacherPosition teacherPosition) {
+        return teacherPositionService.createTeacherPosition(teacherPosition);
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAuthority('positions:update')")
+    public TeacherPosition updateTeacherPosition(@RequestBody TeacherPosition teacherPosition) {
+        return teacherPositionService.updateTeacherPosition(teacherPosition);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('positions:delete')")
+    public ResponseEntity<?> deleteTeacherPosition(@PathVariable UUID id) {
+        try {
+            teacherPositionService.deleteTeacherPosition(id);
+        } catch (RuntimeException exception) {
+            log.error(exception.getMessage());
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(true);
     }
 }
--- src/main/java/com/bntu/timetable/controller/teacher/WorkTariffController.java
+++ src/main/java/com/bntu/timetable/controller/teacher/WorkTariffController.java
@@ -4,13 +4,13 @@ import com.bntu.timetable.entity.teacher.WorkTariff;
 import com.bntu.timetable.service.api.teacher.WorkTariffService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -25,5 +25,29 @@ public class WorkTariffController {
     @PreAuthorize("hasAuthority('worktariffs:read')")
     public List<WorkTariff> getWorkTariffs() {
         return workTariffService.getWorkTariffs();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('worktariffs:create')")
+    public WorkTariff createWorkTariff(@RequestBody WorkTariff workTariff) {
+        return workTariffService.createWorkTariff(workTariff);
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAuthority('worktariffs:update')")
+    public WorkTariff updateWorkTariff(@RequestBody WorkTariff workTariff) {
+        return workTariffService.updateWorkTariff(workTariff);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('worktariffs:delete')")
+    public ResponseEntity<?> deleteWorkTariff(@PathVariable UUID id) {
+        try {
+            workTariffService.deleteWorkTariff(id);
+        } catch (RuntimeException exception) {
+            log.error(exception.getMessage());
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.ok(true);
     }
 }
